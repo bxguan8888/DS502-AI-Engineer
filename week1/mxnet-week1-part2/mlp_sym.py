2bcaaded0c32488f2b9245a4a47c85109e10c050
Boxuan Guan <boxuanguan@gmail.com>
2017-10-18 21:15:09 -0400

    Week1-Part2 Assignment

--- week1/mxnet-week1-part2/mlp_sym.py
+++ week1/mxnet-week1-part2/mlp_sym.py
@@ -44,7 +44,7 @@ def get_mlp_sym():
     return mlp
 
 
-def conv_layer(if_pool=False):
+def conv_layer(input_layer, kernel=(5, 5), num_filter=20, is_pool=False, pool_kernel=(2,2)):
     """
     :return: a single convolution layer symbol
     """
@@ -54,19 +54,43 @@ def conv_layer(if_pool=False):
     # Do you need pooling?
     # What is the expected output shape?
 
-    pass
+    conv = mx.sym.Convolution(data=input_layer, kernel=kernel, num_filter=num_filter)
+    relu = mx.sym.Activation(data=conv, act_type="relu")
+    if is_pool:
+        return mx.sym.Pooling(data=relu, pool_type="max", kernel=pool_kernel, stride=pool_kernel)
+    else:
+        return relu
 
 
-# Optional
-def inception_layer():
+# # Optional
+def inception_layer(input_layer):
     """
     Implement the inception layer in week3 class
     :return: the symbol of a inception layer
     """
-    pass
+    covn1 = conv_layer(input_layer, kernel=(1, 1))
+    covn1_flatten = mx.symbol.Flatten(data=covn1)
+
+    covn2 = conv_layer(input_layer, kernel=(1, 1))
+    covn2 = conv_layer(covn2, kernel=(3, 3))
+    covn2_flatten = mx.symbol.Flatten(data=covn2)
+
+    covn3 = conv_layer(input_layer, kernel=(1, 1))
+    covn3 = conv_layer(covn3, kernel=(4, 4))
+    covn3_flatten = mx.symbol.Flatten(data=covn3)
+
+    covn4 = mx.sym.Pooling(data=input_layer, pool_type="max", kernel=(3, 3), stride=(3, 3))
+    covn4 = conv_layer(covn4, kernel=(1, 1))
+    covn4_flatten = mx.symbol.Flatten(data=covn4)
 
 
-def get_conv_sym():
+    # inception_output = mx.sym.Concat(*[covn1, covn2, covn3, covn4])
+    inception_output = mx.sym.concat(covn1_flatten, covn2_flatten, covn3_flatten, covn4_flatten)
+    return inception_output
+
+
+
+def get_conv_sym(use_inception=False):
 
     """
     :return: symbol of a convolutional neural network
@@ -77,3 +101,21 @@ def get_conv_sym():
     # How wide the network do you want? like 32/64/128 kernels per layer
     # How is the convolution like? Normal CNN? Inception Module? VGG like?
     pass
+
+    conv1 = conv_layer(data, is_pool=True)
+    conv2 = conv_layer(conv1, is_pool=True)
+
+    if use_inception:
+        intermediate_output = inception_layer(conv2)
+    else:
+        intermediate_output = conv2
+
+    flatten = mx.symbol.Flatten(data=intermediate_output)
+    fc1 = mlp_layer(input_layer=flatten, n_hidden=128, activation="relu", BN=True)
+    fc2 = mlp_layer(input_layer=fc1, n_hidden=64, activation="relu", BN=True)
+
+    # MNIST has 10 classes
+    l = mx.sym.FullyConnected(data=fc2, num_hidden=10)
+    # Softmax with cross entropy loss
+    cnn = mx.sym.SoftmaxOutput(data=l, name='softmax')
+    return cnn
--- week1/mxnet-week1-part2/train_mlp.py
+++ week1/mxnet-week1-part2/train_mlp.py
@@ -2,8 +2,10 @@ import mxnet as mx
 import numpy as np
 import cv2
 import logging
+import sys
 from data import get_mnist
 from mlp_sym import get_mlp_sym
+from mlp_sym import get_conv_sym
 logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
 
 if __name__=="__main__":
@@ -13,15 +15,18 @@ if __name__=="__main__":
     train_iter, val_iter = get_mnist(batch_size)
 
     # Get symbol
-    model = get_mlp_sym()
-    # todo: model = get_conv_sym()
+    #
+    if sys.argv[1] == 'true':
+        model = get_conv_sym(sys.argv[2] == 'true')
+    else:
+        model = get_mlp_sym()
 
     # Viz the graph and save the plot for debugging
     plot = mx.viz.plot_network(model, title="mlp", save_format="pdf", hide_weights=True)
     plot.render("MLP")
 
     # create a trainable module on CPU/GPU
-    mod = mx.mod.Module(symbol=model, context=mx.cpu())
+    mod = mx.mod.Module(symbol=model, context=mx.gpu())
     mod.fit(train_iter,  # train data
             eval_data=val_iter,  # validation data
             optimizer='sgd',  # use SGD to train
@@ -30,4 +35,3 @@ if __name__=="__main__":
             batch_end_callback=mx.callback.Speedometer(batch_size, 100),
             # output progress for each 100 data batches
             num_epoch=10)  # train for at most 10 dataset passes
-
